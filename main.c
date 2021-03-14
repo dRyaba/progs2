23d2f6d9a84c3cf96e69b4bc0c64d6cb5f6a2958
dRyaba <79122925+dRyaba@users.noreply.github.com>
2021-03-14 23:47:20 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 #define SIZE 255
 
 int main() {
     int l, c;//line, column
     scanf("%d%d", &c, &l);
-    int a[SIZE][SIZE], max, same = c;
+    int a[SIZE][SIZE], max, same = c, left, mid, right;
     for (int i = 0; i < l; i++) {
         for (int j = 0; j < c; j++) {
             scanf("%d", &a[i][j]);
@@ -13,9 +14,16 @@ int main() {
     for (int i = l - 2; i >= 0; i--) {
         for (int j = c - 1; j >= 0; j--) {
             max = a[i][j];
-            for (int m = ((j - 1) < 0 ? 0 : j - 1); m <= ((j + 1) < c ? j + 1 : c); m++) {
-                max = ((max > a[i + 1][m]) ? max : a[i + 1][m]);
-            }
+            left = a[i + 1][j - 1];
+            mid = a[i + 1][j];
+            right = a[i + 1][j + 1];
+            if (max < left) {
+                max = left;}
+            if (max < mid) {
+                max = mid;}
+            if (max < right) {
+                max = right;}
+
             same += (a[i][j] == max);
             a[i][j] = max;
         }
